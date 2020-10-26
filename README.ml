3f98e460cd15fd1bff4d7cba860a0df53308bd7b
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2020-10-26 18:59:55 +0100

    Initial commit
    Contains a fresh rust project with the initial Hello world and a README.md

---
+++ README.ml
@@ -0,0 +1,5 @@
+#Pwnage Checker
+
+This is eventually meant to be a simple program that checks if passwords have been PWNED.
+After a provided password has been hashed, the program will try to find the hash among a list of hashed leaked passwords.
+It is meant to be used with password files downloaded from [have i been pwned?](https://haveibeenpwned.com/) and perform a binary search on the lines.
