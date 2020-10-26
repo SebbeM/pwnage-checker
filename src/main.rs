67efd610e55e7330e5a77a319b813e1661f0bfc9
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2020-10-26 19:28:05 +0100

    Read arguments
    Reads two arguments from the command line. The first will point to the
    file to search and the other will be optional and tell the program
    to use binary search.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    /// The file to search
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+    /// Option to perform binary search (requires sorted file)
+    binary: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("{}", args.binary);
 }
