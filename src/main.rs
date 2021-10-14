5ba7413d7e8e2ea43db0408aec28bf3199e0930c
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2021-10-14 21:35:12 +0200

    Start over coding from scratch
    After a year since the last commit, I completely forgot what I was
    doing. Starting over using the excellent tutorial found at:
    https://doc.rust-lang.org/book/ch12-00-an-io-project.html

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::fs::File;
-
-use structopt::StructOpt;
-
-#[derive(StructOpt)]
-struct Cli {
-    /// The file to search
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
-    /// Option to perform binary search (requires sorted file)
-    binary: String,
-}
+use std::env;
+use std::fs;
 
 fn main() {
-    let args = Cli::from_args();
-    let f = File::open(&args.path).expect("failed to read file");
-    let mut reader = BufReader::new(f);
+    let args: Vec<String> = env::args().collect();
 
-    let mut line = String::new();
-    let content = reader.read_line(&mut line);
-    println!("First line: {}", line);
+    let pass = &args[1];
+    let file = &args[2];
+
+    println!("Searching for {}", pass);
+    println!("In file {}", file);
+
+    let contents = fs::read_to_string(file)
+        .expect("Something went wrong when reading the file");
+
+    println!("With text:\n{}", contents);
 }
