a31b9b0ac3c085dddda71e8918535d57c51df124
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2026-02-09 13:51:55 +0100

    Count iterations

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ use std::cmp::Ordering;
 use std::convert::TryFrom;
 use std::convert::TryInto;
 use std::error::Error;
+use std::f32::consts::LOG10_2;
 use std::fs::File;
+use std::iter;
 use std::os::unix::fs::FileExt;
 use std::u64;
 
@@ -25,17 +27,24 @@ impl Params {
 }
 
 pub fn run(params: Params) -> Result<(), Box<dyn Error>> {
-    let hashed_pass = format!("{:X}", Sha1::digest(params.pass));
-    let pass: [u8; 40] = hashed_pass.as_bytes().try_into().unwrap();
+    let hashed_pass = format!("{:X}", Sha1::digest(params.pass.clone()));
+    let search_token: [u8; 40] = hashed_pass.as_bytes().try_into().unwrap();
 
-    let mut file: File = File::open(params.path.clone()).unwrap();
+    let file: File = File::open(params.path.clone()).unwrap();
 
     let len = file.metadata().unwrap().len();
-    println!("The file is {} bytes long", len);
+    println!(
+        "The file is {} bytes long. The search should take aproximately {} iterations.",
+        len,
+        len.ilog2()
+    );
 
-    match binary_search(file, pass, len) {
-        Some(index) => println!("Binary search found at index: {}", index),
-        None => println!("Password \"{}\" not found", "password"),
+    match binary_search(file, search_token, len) {
+        Some(index) => println!(
+            "Binary search found \"{}\" at index: {}",
+            params.pass, index
+        ),
+        None => println!("Password \"{}\" not found", params.pass),
     }
 
     Ok(())
@@ -44,9 +53,11 @@ pub fn run(params: Params) -> Result<(), Box<dyn Error>> {
 fn binary_search(file: File, token: [u8; 40], end: u64) -> Option<usize> {
     let mut low: u64 = 0;
     let mut high: u64 = u64::try_from(end - 1).unwrap();
+    let mut iterations: u32 = 0;
     let buf: &mut [u8; 40] = &mut [0; 40];
 
     while low <= high {
+        iterations += 1;
         let mid: u64 = (low + high) / 2;
         eprintln!("Middle is now at {}", mid);
 
@@ -57,15 +68,22 @@ fn binary_search(file: File, token: [u8; 40], end: u64) -> Option<usize> {
             let offset = mid + new_line - 40;
             res = file.read_exact_at(buf, offset);
         };
+
         match res {
-            Err(e) => println!(
-                "Failed to read {} bytes at position {} due to error {}.\n Breaking!",
-                buf.len(),
-                mid,
-                e
-            ),
+            Err(e) => {
+                println!(
+                    "Failed to read {} bytes at position {} due to error {}.",
+                    buf.len(),
+                    mid,
+                    e
+                );
+                return None;
+            }
             Ok(_) => match token.cmp(buf) {
-                Ordering::Equal => return Some(usize::try_from(mid).unwrap()),
+                Ordering::Equal => {
+                    println!("Found token {:?} after {} iterations", token, iterations);
+                    return Some(usize::try_from(mid).unwrap());
+                }
                 Ordering::Less => {
                     println!("{:?} is greater than {:?}", token, buf);
                     high = mid - 1
@@ -77,6 +95,5 @@ fn binary_search(file: File, token: [u8; 40], end: u64) -> Option<usize> {
             },
         }
     }
-
     None
 }
