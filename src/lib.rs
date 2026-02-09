28c1b6ccc835d312a1194d33f0d02ae4bf30d835
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2026-02-09 13:20:52 +0100

    Rewrite until it actually works!

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-use std::fs::File;
-use std::error::Error;
-use std::io::BufRead;
-use std::io::BufReader;
-use std::io::Seek;
-use std::io::SeekFrom;
+use sha1::{Digest, Sha1};
 use std::cmp::Ordering;
 use std::convert::TryFrom;
+use std::convert::TryInto;
+use std::error::Error;
+use std::fs::File;
+use std::os::unix::fs::FileExt;
+use std::u64;
 
 pub struct Params {
     pub pass: String,
@@ -15,7 +15,7 @@ pub struct Params {
 impl Params {
     pub fn new(args: &[String]) -> Result<Params, &str> {
         if args.len() < 3 {
-            return Err("Too few arguments.\nPlease provide a search term and a file name.")
+            return Err("Too few arguments.\nPlease provide a search term and a file name.");
         }
         let pass = args[1].clone();
         let path = args[2].clone();
@@ -25,42 +25,58 @@ impl Params {
 }
 
 pub fn run(params: Params) -> Result<(), Box<dyn Error>> {
-    let mut file = File::open(params.path.clone())?;
-    let len = i64::try_from(file.seek(SeekFrom::End(0)).unwrap()).unwrap();
+    let hashed_pass = format!("{:X}", Sha1::digest(params.pass));
+    let pass: [u8; 40] = hashed_pass.as_bytes().try_into().unwrap();
+
+    let mut file: File = File::open(params.path.clone()).unwrap();
+
+    let len = file.metadata().unwrap().len();
     println!("The file is {} bytes long", len);
-    let reader = BufReader::new(file);
-    println!("Binary search found: {}", bin_search(&params.pass, reader, len));
+
+    match binary_search(file, pass, len) {
+        Some(index) => println!("Binary search found at index: {}", index),
+        None => println!("Password \"{}\" not found", "password"),
+    }
 
     Ok(())
 }
 
-fn bin_search(pass: &str, mut reader: BufReader<File>, step: i64) -> String {
-    reader.seek(SeekFrom::Current(step));
+fn binary_search(file: File, token: [u8; 40], end: u64) -> Option<usize> {
+    let mut low: u64 = 0;
+    let mut high: u64 = u64::try_from(end - 1).unwrap();
+    let buf: &mut [u8; 40] = &mut [0; 40];
 
-    let mut hash = pass.to_string();
+    while low <= high {
+        let mid: u64 = (low + high) / 2;
+        eprintln!("Middle is now at {}", mid);
 
-    reader.read_line(&mut hash);
-    if hash.cmp(&pass.to_string()) == Ordering::Less {
-        hash = bin_search(pass, reader, -step / 2);
-    } else if hash.cmp(&pass.to_string()) == Ordering::Greater {
-        hash = bin_search(pass, reader, step / 2);
-    } 
-    hash
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn one_result() {
-        let pass = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.";
-
-        assert_eq!(vec!["safe, fast, productive."], search(pass, contents));
+        let mut res = file.read_exact_at(buf, mid);
+        let newline_index = buf.iter().position(|&r| r == b':');
+        if newline_index.is_some() {
+            let new_line: u64 = newline_index.unwrap().try_into().unwrap();
+            let offset = mid + new_line - 40;
+            res = file.read_exact_at(buf, offset);
+        };
+        match res {
+            Err(e) => println!(
+                "Failed to read {} bytes at position {} due to error {}.\n Breaking!",
+                buf.len(),
+                mid,
+                e
+            ),
+            Ok(_) => match token.cmp(buf) {
+                Ordering::Equal => return Some(usize::try_from(mid).unwrap()),
+                Ordering::Less => {
+                    println!("{:?} is greater than {:?}", token, buf);
+                    high = mid - 1
+                }
+                Ordering::Greater => {
+                    println!("{:?} is less than {:?}", token, buf);
+                    low = mid + 1
+                }
+            },
+        }
     }
+
+    None
 }
